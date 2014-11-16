f06ed3decbaa4ba9de5bfe01af5dc3352648724f
Tim Dorr <timdorr@timdorr.com>
2014-11-15 23:31:49 -0500

    Create tickets for realsies.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,5 +3,25 @@ class TicketsController < ApplicationController
   end
 
   def create
+    @ticket = ticket
+  end
+
+  private
+
+  def ticket_fields
+    {
+        comment: {
+            body: params[:body]
+        },
+        requester: {
+            locale_id: 1,
+            name: "Tim Dorr",
+            email: "tim.dorr@salesloft.com"
+        }
+    }
+  end
+
+  def ticket
+    $zendesk.tickets.create(ticket_fields)
   end
 end

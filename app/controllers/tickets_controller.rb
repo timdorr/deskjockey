67ab776ddeda4f1668ed0dacbcc272ead8d7d075
Tim Dorr <timdorr@timdorr.com>
2014-11-16 14:26:10 -0500

    Pass custom fields to Zendesk.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -17,8 +17,13 @@ class TicketsController < ApplicationController
             locale_id: 1,
             name: session[:name],
             email: session[:email]
-        }
+        },
+        custom_fields: ticket_custom_fields
     }
+  end
+
+  def ticket_custom_fields
+    session[:fields].map{ |id, value| {id: id.to_i, value: value} }
   end
 
   def ticket

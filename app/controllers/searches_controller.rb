b67cf011401a8502d714c9229532184c54d7cfab
Tim Dorr <timdorr@timdorr.com>
2014-11-15 22:47:16 -0500

    Search the KB and show the results.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,20 @@ class SearchesController < ApplicationController
   end
 
   def create
+    @results = search_results["results"]
+  end
+
+  private
+
+  def search_limit
+    5
+  end
+
+  def search_params
+    params.slice(:query, :category).merge(per_page: search_limit)
+  end
+
+  def search_results
+    $zendesk.connection.get("help_center/articles/search.json", search_params).body
   end
 end

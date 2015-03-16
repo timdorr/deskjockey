15dec7b4707e2cf33d81098e035d6a8cfcc0083b
Ben Olive <ben.olive@salesloft.com>
2015-03-16 15:28:51 -0400

    Add `search` JS api method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :searches
+  resources :searches, only: [:new]
   resources :tickets
+
+  match "/searches", to: "searches#create", via: [:get, :post]
 
   get "/widget.js", to: redirect{|| ActionController::Base.helpers.asset_path("widget.js") }
 
